df679997b75be05ada11252039971a8ff4367feb
akani28 <nietocevallos61@gmail.com>
2023-08-01 03:23:27 -0500

    db and function randomPharse

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 
 import './App.css'
+import quotes from './db/quotes.json'
 
 function App() {
+  const getRandom= (list) =>{
+    const randomIndex = Math.floor(Math.random()*list.length) 
+    return list[randomIndex];
+  }
+  console.log(getRandom(quotes));
  
 
   return (
