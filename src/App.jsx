f2c48904194087ceabe60df2bc428bd3259f87d0
akani28 <nietocevallos61@gmail.com>
2023-08-02 01:57:01 -0500

    backgroundImage and aplied styles

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,20 @@ import quotes from './db/quotes.json'
 import { getRandom } from './utils/random';
 import QuoteBox from './components/QuoteBox.jsx'
 
+const listBg = ["bg1", "bg2", "bg3", "bg4"]
 
 function App() {
   
   const [quote, setQuote] = useState(getRandom(quotes))
+  const [currentBg, setCurrentBg] = useState(getRandom(listBg))
 
   const handleChangeQuote = () =>{
     setQuote(getRandom(quotes))
+    setCurrentBg(getRandom(listBg))
 
   }
- 
-
   return (
-    <main>
+    <main className={`App ${currentBg}` }>
       <QuoteBox handleChangeQuote={handleChangeQuote} quote={quote}/>
     </main>
   )
--- src/components/QuoteBox.jsx
+++ src/components/QuoteBox.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./styles/QuoteBox.css"
 
 const QuoteBox = ({handleChangeQuote, quote}) => {
   return (
